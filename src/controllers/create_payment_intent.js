5336b123fedda3aae67d659a53e42d8706e1a4b0
jubayer <jubayerahmed.dev@gmail.com>
2025-03-11 12:03:56 +0600

    create stripe instance to client_secret and send client side

--- src/controllers/create_payment_intent.js
+++ src/controllers/create_payment_intent.js
@@ -1,9 +1,22 @@
+import "dotenv/config";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.stripe_sk);
+
 const createPaymentIntent = async (req, res) => {
-  const body = req.body;
+  const { perMonth, monthDuration } = req.body;
 
-  console.log(body);
+  const totalPaymentInCent = parseFloat(perMonth) * monthDuration * 100;
 
-  res.send({ perMonth: { h: "hello" } });
+  const { client_secret } = await stripe.paymentIntents.create({
+    amount: totalPaymentInCent,
+    currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({ client_secret });
 };
 
 export { createPaymentIntent };
