b8fcd16ed9702360c7c088a429d82864f19e0ae0
jubayer <jubayerahmed.dev@gmail.com>
2025-03-18 17:22:34 +0600

    mongo uri changed

--- src/middlewares/global_error.js
+++ src/middlewares/global_error.js
@@ -1,6 +1,4 @@
 const globalError = (err, req, res, next) => {
-  console.log(err);
-
   const message = err.message ?? "Internal Server Error";
   const status = err.status ?? 500;
 
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,7 +2,7 @@ import cors from "cors";
 import express from "express";
 import { globalError } from "./global_error.js";
 
-const CLIENT_SIDE_URL = "http://localhost:5173";
+const CLIENT_SIDE_URL = "https://synapse-776a2.web.app";
 
 const applyMiddlewares = (app) => {
   app.use(
