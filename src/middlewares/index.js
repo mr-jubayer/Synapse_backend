106561122f5e399c468e02898ce168a86c5ffbe8
jubayer <jubayerahmed.dev@gmail.com>
2025-03-19 11:49:50 +0600

    local url for testing

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -3,11 +3,12 @@ import express from "express";
 import { globalError } from "./global_error.js";
 
 const CLIENT_SIDE_URL = "https://synapse-776a2.web.app/";
+const local_url = "http://localhost:5173/";
 
 const applyMiddlewares = (app) => {
   app.use(
     cors({
-      origin: CLIENT_SIDE_URL,
+      origin: [CLIENT_SIDE_URL, local_url],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     })
   );
